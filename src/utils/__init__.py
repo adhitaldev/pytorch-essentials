3ae6f450084fd4ad102ad315852bbf3bc1b57dee
Ashish Dhital <ashish_dhital@trimble.com>
2026-08-17 15:34:38 -0600

    Utils to generate mock csv data and to create tensor out of csv files

---
+++ src/utils/__init__.py
@@ -0,0 +1,2 @@
+
+from .csv_to_tensor import get_tensor_from_csv
---
+++ src/utils/csv_to_tensor.py
@@ -0,0 +1,25 @@
+"""
+Takes in a CSV file and converts it to a tensor.
+"""
+import torch
+
+
+def get_tensor_from_csv(csv_file_path: str, delimiter: str = ",", has_header: bool = True) -> torch.tensor:
+    """
+    Reads a CSV file and converts it to a tensor.
+    Args:
+        csv_file_path (str): Path to the CSV file.
+        delimiter (str): Delimiter used in the CSV file. Default is ','.
+    Returns:
+        torch.tensor: Tensor representation of the CSV data.
+    """
+    data = []
+    with open(csv_file_path, 'r') as f:
+        for line in f:
+            if has_header:
+                has_header = False
+                continue
+            row = line.strip().split(delimiter)
+            print(row)
+            data.append([float(x) for x in row])
+    return torch.tensor(data, dtype=torch.float32)
